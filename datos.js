3f6e5d7abb369637124efe9819d3d002a677666b
Jesus <al02811281@tecmilenio.mx>
2023-06-22 16:01:06 -0700

    README

---
+++ datos.js
@@ -0,0 +1,16 @@
+
+//tipo de dato string
+let abecedario = "ABC";
+console.log(abecedario);
+
+//tipo de dato numerico
+let tiktok = 6500;
+console.log(tiktok);
+
+tiktok = 1256;
+console.log(tiktok)
+
+let x , y;
+x = 20 , y = 10;
+z = x + y;
+console.log(z);
